40520b56267d467a1bb5119b52374b9cee22c7e2
Spencer Finkel <finsp01@gmail.com>
2024-08-20 19:06:47 -0400

    modularized config

---
+++ lua/keymaps.lua
@@ -0,0 +1,65 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- [[ the most important
+vim.keymap.set('i', 'jk', '<Esc>l')
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- [[ copy and paste key remap
+-- Remap Ctrl+C to copy to both "+ and "* registers
+vim.api.nvim_set_keymap('n', '<C-c>', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
+
+-- Remap Ctrl+V to paste from "+ register
+vim.api.nvim_set_keymap('n', '<C-v>', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-v>', '<C-r>+', { noremap = true, silent = true })
+
+-- Additional mappings to use "* register
+vim.api.nvim_set_keymap('n', '<Leader>y', '"*y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Leader>y', '"*y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>p', '"*p', { noremap = true, silent = true })
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,53 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {
+    options = {
+      mode = 'buffers',
+      style_preset = function()
+        local bufferline = require 'bufferline'
+        return bufferline.style_preset.default
+      end,
+      numbers = 'none',
+      close_command = 'bdelete! %d',
+      right_mouse_command = 'bdelete! %d',
+      left_mouse_command = 'buffer %d',
+      middle_mouse_command = nil,
+      indicator = {
+        -- icon = '▎',
+        style = 'underline',
+      },
+      buffer_close_icon = '󰅖',
+      modified_icon = '●',
+      close_icon = '',
+      left_trunc_marker = '',
+      right_trunc_marker = '',
+      name_formatter = function(buf)
+        return buf.name
+      end,
+      diagnostics = 'nvim_lsp',
+      diagnostics_update_on_event = true,
+      offsets = {
+        {
+          filetype = 'NvimTree',
+          text = 'File Explorer',
+          text_align = 'center',
+          separator = true,
+        },
+      },
+      color_icons = true,
+      show_buffer_icons = true,
+      show_bugger_close_icons = true,
+      show_close_icon = true,
+      show_tab_indicators = true,
+      seperator_style = 'slant',
+      enforce_regular_tabs = true,
+      always_show_bufferline = true,
+      hover = {
+        enabled = true,
+        reveal = { 'close' },
+      },
+    },
+  },
+}
